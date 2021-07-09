37a34f325c6375746e1704bb1292afc12661d73e
surabhikanade <kanadesurabhi59@gmail.com>
2021-07-09 13:41:17 +0530

    add

--- Array/array1.cpp
+++ Array/array1.cpp
@@ -4,17 +4,17 @@ using namespace std;
 int main()
 {
     int n;
-    cin >> n;
+    std::cin >> n;
 
     int array[n];
 
     for (int i = 0; i < n; i++)
     {
-        cin >> array[i];
+        std::cin>> array[i];
     }
 
     for (int i = 0; i < n; i++)
     {
-        cout << array[i]<<" ";
+        std::cout << array[i]<<" ";
     }
 }
